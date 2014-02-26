c2d3fbee35e9055aa334ca6cc3825d216a4a6e55
Kelt Dockins <kelt@dockins.org>
2014-02-25 19:15:33 -0600

    fixing bug with sprockets filter

--- src/Codesleeve/Sprockets/SprocketsFilter.php
+++ src/Codesleeve/Sprockets/SprocketsFilter.php
@@ -41,7 +41,12 @@ class SprocketsFilter implements FilterInterface
         }
 
         $global_filters = $this->parser->get("sprockets_filters.{$this->parser->mime}", array());
-        $global_filters = array_merge($global_filters, array(new Filters\JavascriptConcatenationFilter));
+
+        // handle ASI issue with javascripts
+        if ($this->parser->mime == "javascripts")
+        {
+            $global_filters = array_merge($global_filters, array(new Filters\JavascriptConcatenationFilter));
+        }
 
         $collection = new AssetCollection($files, $global_filters);
 
